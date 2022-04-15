73aa0fe9d853858d35c1743019f8d732224c57cb
Habibur96 <habiburrahmannayan66@gmail.com>
2022-04-16 00:43:08 +0600

    calculator part added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 
+
+// Generate Pin Part
 function getPin() {
     const pin = Math.round(Math.random() * 10000)
     const pinString = pin + ''
@@ -15,3 +17,22 @@ function generatePin() {
     const pin = getPin()
     document.getElementById('display-pin').value = pin;
 }
+
+// Calculator Part
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
+});
+
